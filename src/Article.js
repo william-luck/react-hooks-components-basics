4542cd5f8eca31a9e01c08473bc8e7af42aa8647
William Luck <luck.william.h@gmail.com>
2022-05-11 13:30:57 -0500

    commit tree ca6228836f4128a96db517472eb2eb8d805da956

--- src/Article.js
+++ src/Article.js
@@ -5,5 +5,6 @@ function Article() {
     <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
   );
 }
+// Function has a return value of JSX, which telsl React "when you want to put this componenet on the DOM, here is what it should become"
 
 export default Article;
